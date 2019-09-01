ed9795431cbd50536454e79f0d10e2f24fb70776
Adam Weissman <adam@comedytragedyepic.com>
2019-09-01 14:15:35 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,17 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
+def the_rows
+  return "   |   |   "
+end
+  
+def the_separators
+  return "-----------"
+end
+
 def display_board
-  puts "   |   |   "
-  puts 
+  the_rows
+  the_separators
+  the_rows
+  the_separators
+  the_rows
+end
